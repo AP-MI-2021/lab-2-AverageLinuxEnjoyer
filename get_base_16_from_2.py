ff92e630e234d15690f0aaa890a720466ba981b6
nullptr <galanlucian002@gmail.com>
2021-10-05 22:11:47 +0300

    reparat o greseala

--- get_base_16_from_2.py
+++ get_base_16_from_2.py
@@ -23,20 +23,22 @@ def get_base_16_from_2(n: str) -> str:
     digits = '0123456789ABCDEF'
     while num:
         new_n += digits[num % base]
-        num //= 10
+        num //= 16
 
-    return new_n
+    return new_n[::-1]
 
+    
 def test_get_base_16_from_2():
     """Testeaza functia get_base_16_from_2
     """
     assert get_base_16_from_2("101") == '5'
     assert get_base_16_from_2("0") == '0'
     assert get_base_16_from_2("1") == '1'
-    assert get_base_16_from_2("0101011") == 'B4'
-
-test_get_base_16_from_2()
+    assert get_base_16_from_2("0101011") == '2B'
+    
 
 def input_get_base_16_from_2():
     x = input("Functia converteste un numar din baza 2 in baza 16. Ce numar sa fie convertit? ")
     print(f"Numarul in baza 2: {x}, convertit in baza 16, este: {get_base_16_from_2(x)}.")
+
+input_get_base_16_from_2()
